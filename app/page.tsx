591cddbe631584c3d6969f920783fd9763cded69
nicolas.puchois <nicolas.puchois@laposte.net>
2026-05-06 15:33:48 +0200

    - ajout de la structure de la page d'accueil - ajout des variables de style global ainsi que le border radius

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,56 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <body>
+      
+      <main>
+        <section>
+          <pre> 
+            {`const Nicolas = {
+            Work_Post:      ['Backend_developper', 'Full-stack_developper', 'IT Support'],
+            Hobbies:        ['History', 'Video Games', 'Learn new things'],
+            Ready_to_work:  true
+            }`}
+          </pre>
+          <img src="" alt="" />
+        </section>
+
+              <h1>Test</h1>
+
+        <section>
+          <h2>A propos</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, ratione soluta? Facere vitae tenetur dignissimos dolores, aliquid qui ad, ut quo quisquam neque illo quas explicabo doloribus nesciunt. Laborum, harum.</p>
+        </section>
+
+
+        <section>
+          <h2> Mes projets</h2>
+          <div></div>
+          <div></div>
+
+          <button> Pour voir l&apos;ensemble de mes projets, cliquer ici</button>
+        </section>
+
+        <section>
+          <h2> Mes compétences</h2>
+          <section>
+            <h3> Language</h3>
+          </section>
+          <section>
+            <h3>Framework et librairies</h3>
+          </section>
+          <section>
+            <h3>logiciel</h3>
+          </section>
+        </section>
+        <section>
+          <h2>Contact</h2>
+          <a href="mailto:">Me contacter par mail</a>
+          <a href=""> Mon GitHub</a>
+          <a href="">Mon linkedin</a>
+        </section>
       </main>
-    </div>
+    </body>
+
   );
 }
